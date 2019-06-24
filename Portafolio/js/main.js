91963d8e1029ee75680f0fef2453f2b26f4914ea
Luisfelce1 <47182878+Luisfelce1@users.noreply.github.com>
2019-06-24 17:44:33 -0400

    Add files via upload

--- Portafolio/js/main.js
+++ Portafolio/js/main.js
@@ -1,4 +1,26 @@
-$(document).ready(function(){
-alert(Hola);
+$(document).  ready(function(){
 
+    var banner={
+        padre: $('#banner'),
+        numeroSlides: $('#banner').children('.slide').length,
+        posicion: 1
+    }
+
+    banner.padre.children('.slide').first().css({
+        'left':0
+    });
+
+    var altoBanner = function(){
+        var alto= banner.padre.children('.slide').outerheight();
+        banner.padre.css({
+            'height': alto + 'px'
+
+        });
+        console.log(alto);
+    }
+    altoBanner();
+
+    $(window).resize(function(){
+        altoBanner();
+    });
 });
